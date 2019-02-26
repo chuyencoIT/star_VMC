c5ac5eba62ce52b64ea4a08fa74b22ff55509b08
chuyencoIT <40766686+chuyencoIT@users.noreply.github.com>
2019-02-26 21:18:15 +0700

    Add files via upload

---
+++ Home.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="vi">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>LEARN STARTUP</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    
+   
+    </head>
+      <div class="navbar">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#"><i class="fas fa-star"></i></a>
+          <div class="navbar-brand-left">
+          <ul class="nav navbar-nav">
+            <li class="active">
+              <a href="#">TÀI LIỆU IT</a>
+            </li>
+            <li>
+              <a href="#">KHO SÁCH</a>
+            </li>
+            <li class="active">
+              <a href="#">KỸ NĂNG SỐNG</a>
+            </li>
+          </ul>
+          </div>
+          
+       </div>
+      </div>
+  <body>
+    <div class="main container">
+      
+        <div id="carousel-id" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carousel-id" data-slide-to="0" class=""></li>
+            <li data-target="#carousel-id" data-slide-to="1" class=""></li>
+            <li data-target="#carousel-id" data-slide-to="2" class="active"></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="item">
+              <img src="img/img1.png">
+             
+            </div>
+            <div class="item">
+              <img src="img/img2.png">
+              
+            </div>
+            <div class="item active">
+              <img src="img/img3.png">
+               
+            </div>
+          </div>
+          <a class="left carousel-control" href="#carousel-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+          <a class="right carousel-control" href="#carousel-id" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+        </div>
+      <div class="row">
+        <div class="col-sm-6 col-md-4">
+          <div class="thumbnail">
+            <img src="img/js.jpg">
+            <h3>Tutorial JS beginer</h3>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4">
+          <div class="thumbnail">
+            <img src="img/mongDB.jpg">
+            <h3>Introduction DB</h3>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4">
+          <div class="thumbnail">
+            <img src="img/nodejs.jpeg">
+            <h3>Introduction NODE.JS</h3>
+          </div>
+        </div>
+
+  </div>
+  <div class="book">
+     <h3 class="text"><b>BEST-BOOK</b></h3>
+     <div class="row">
+        <div class="col-sm-6 col-md-4">
+          <div class="thumbnail">
+            <img src="img/nha-gia-kim.jpg">
+            <h4>NHÀ GIẢ KIM</h4>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 book">
+          <div class="thumbnail">
+            <img src="img/hanhtrinhvephuongdong.jpg">
+            <h4>HÀNH TRÌNH VỀ PHƯƠNG ĐÔNG</h4>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4">
+          <div class="thumbnail">
+            <img src="img/banguoithayvidai.jpg">
+            <h4>BA NGƯỜI THẦY VĨ ĐẠI</h4>
+          </div>
+        </div>
+
+  </div> 
+    
+  </div>
+  <div class="life">
+     <h3 class="text"><b>THE SCHOOL OF LIFE</b></h3>
+     
+     <div class="row">
+        <div class="col-xs-6 col-md-3">
+          <div class="thumbnail">
+            <iframe width="241" height="180" src="https://www.youtube.com/embed/V3WrCx3mwNo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <h4>GET UP & GET IT DONE</h4>
+          </div>
+        </div>
+        <div class="col-xs-6 col-md-3">
+          <div class="thumbnail">
+            <iframe width="241" height="180" src="https://www.youtube.com/embed/wsNzAuYDgy0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <h4>STOP WASTING TIME</h4>
+          </div>
+        </div>
+        <div class="col-xs-6 col-md-3">
+          <div class="thumbnail">
+            <iframe width="241" height="180" src="https://www.youtube.com/embed/CB9GhE2Jk98" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <h4>The Challenge of Eating Alone In Public</h4>
+          </div>
+        </div>
+        <div class="col-xs-6 col-md-3">
+          <div class="thumbnail">
+            <iframe width="241" height="180" src="https://www.youtube.com/embed/LGinimRIl04" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <h4>How to Be a Good Friend</h4>
+          </div>
+        </div>
+  </div> 
+    
+  </div>
+  
+</div>
+   
+    <script src="//code.jquery.com/jquery.js"></script>
+    <!-- Bootstrap JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+     
+  </body>
+</html>
